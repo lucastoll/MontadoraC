b3b81f78c5df0511884cd8cdee4b0f2c30ed49e8
lucastoll <lucasfernandestolotto@gmail.com>
2021-11-09 17:38:26 -0300

    menu + tabela
    menu primeira versão feito
    função cadastro e mostra com o char 'x' na tabela funcionando.

--- ProjetoMontadora.c
+++ ProjetoMontadora.c
@@ -3,9 +3,22 @@
 // Pedro Henrique Todineyb Santos
 // Rafael Ramos do Rosário
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <locale.h>
+// ponteiro -> membro da estrutura
+// estrutura (membro) . dados da estrutura
+
+// duvidas
+// na tabela de visualização como fica o endereço
+// como fica a venda dos carros
+
+
+#include <stdio.h> //Biblioteca padrão de entrada/saída, printf scanf.
+#include <stdlib.h> //emula o prompt de comando, cls pause.
+#include <locale.h> //acentuação / linguagem
+#include <math.h>  //funções matématicas
+#include <ctype.h> //Ascii e manipulação de caracteres to upper to lower
+#include <string.h> //Funções string
+#include <windows.h> //Cores
+
 
 //Estruturas
 
@@ -38,14 +51,18 @@ typedef struct loja{
  struct endereco end;
  int sold;
  int reserved;
- union  dadoscarro  tabela[3];
+ union  dadoscarro  tabela;
 }loja;
+
+
+// loja - endereço // loja - union dadoscarro - struct infocarro
 
 // Funções antes da main - Funções antes da main - Funções antes da main - Funções antes da main - Funções antes da main - Funções antes da main.
 
 void aloca(loja **p,int tam);
 int verifica(); //verifica se existe e retorna o registro (qtde)
 void cadastro(loja *p,int qtde); //cadastro struct loja
+void cadastroend(loja *p);
 void grava(loja *p); //grava os dados no ponteiro
 void mostra(loja *p,int qtde);
 
@@ -56,18 +73,55 @@ main()
 setlocale(LC_ALL, "portuguese");
 loja *ploja=NULL;
 char op;
-int qtde;
-
+int qtde, opmenu, controlemenu=1;
 aloca(&ploja,1);
-qtde=verifica(); // Função que retorna o número do registro
-do{
-	cadastro(ploja,qtde+1);
-	qtde++;
-	printf("\nDeseja continuar <S/N>: ");
-	scanf("%c",&op);
-	fflush(stdin);
-  }while(op!='n' && op!= 'N');
-mostra(ploja,qtde);
+
+do
+{
+    system("cls");
+    qtde=verifica(); // Função que retorna o número do registro
+
+    printf("REGISTRO MONTADORAS\n");
+    printf("\nBem vindo, digite uma das opções abaixo.");
+    printf("\n[1] Consulta geral\n[2] Consulta de vendas\n[3] Registrar montadora\n[4] Fechar programa\n\n");
+    scanf("%i", &opmenu);
+    fflush(stdin);
+
+    switch(opmenu)
+    {
+        case 1: mostra(ploja,qtde);
+        break;
+
+        case 2: printf("juninhopernambucacoococo");
+        system("pause");
+        break;
+
+        case 3: do{
+        cadastro(ploja,qtde+1);
+        qtde++;
+        if(qtde>=5)
+        {
+            break;
+        }
+        printf("\nDeseja continuar <S/N>: ");
+        scanf("%c",&op);
+        fflush(stdin);
+        }while(op!='n' && op!= 'N');
+        mostra(ploja,qtde);
+        break;
+
+        case 4: controlemenu = 0;
+        break;
+
+        default: printf("Opção inválida\a\n");
+        system("pause");
+        break;
+    }
+    printf("\nfim");
+}while(controlemenu==1);
+
+
+
 }//main
 
 // Funções depois da main - Funções depois da main - Funções depois da main - Funções depois da main - Funções depois da main - Funções depois da main.
@@ -86,9 +140,9 @@ if((fptr=fopen("concessionaria.bin","rb"))==NULL)
   return cont;
 else
   {
-  	fseek(fptr,0,2);   //posiciona fptr no final do arquivo
-  	cont=ftell(fptr)/sizeof(loja);
-  	fclose(fptr);     //dentro do else - por conta rb
+  	fseek(fptr,0,2);   // posiciona fptr no final do arquivo
+  	cont=ftell(fptr)/sizeof(loja); //n de bytes do começo do arquivo até a posição atual (fim), dividido pela quantidade de bytes que a estrutura tem, isso da um número inteiro certinho que é o nosso cont, usado nas outras funções como tamanho/qtde
+  	fclose(fptr);    // dentro do else - por conta rb (rb não tem força pra criar, então a ação de fechar só deve acontecer dentro do else, caso o arquivo exista)
   	return cont;
   }
 }
@@ -100,12 +154,51 @@ printf("\nID Registro: %i",p->regloja);
 printf("\nNome: ");
 gets(p->nome);
 fflush(stdin);
+
 printf("CNPJ (Ex: 11.444.777/0001-61): ");
 gets(p->CNPJ);
 fflush(stdin);
 
+cadastroend(p);
+fflush(stdin);
+
+p->sold = 0;
+p->reserved = 0;
+p->tabela.sigla = 'x';
 
 grava(p);
+}
+
+
+void cadastroend(loja *p)
+{
+printf("Logradouro: ");
+gets(p->end.logradouro);
+fflush(stdin);
+
+printf("Bairro: ");
+gets(p->end.bairro);
+fflush(stdin);
+
+printf("CEP: ");
+gets(p->end.CEP);
+fflush(stdin);
+
+printf("Cidade: ");
+gets(p->end.cidade);
+fflush(stdin);
+
+printf("Estado: ");
+gets(p->end.estado);
+fflush(stdin);
+
+printf("Telefone: ");
+gets(p->end.fone);
+fflush(stdin);
+
+printf("E-mail: ");
+gets(p->end.email);
+fflush(stdin);
 }
 
 /*
@@ -118,6 +211,17 @@ typedef struct loja{
  int reserved;
  union  lojacarro  tabela[3];
 }loja;
+
+
+struct endereco {
+ char logradouro[80];
+ char bairro[15];
+ char CEP[10];
+ char cidade[15];
+ char estado[3];
+ char fone[12];
+ char email[40];
+};
 */
 
 
@@ -128,12 +232,12 @@ if((fptr=fopen("concessionaria.bin","ab"))==NULL)
   printf("\nErro ao abrir o arquivo");
 else
   fwrite(p,sizeof(loja),1,fptr);
-fclose(fptr);   //fora do else - por conta do ab
+fclose(fptr);   //fora do else - por conta do ab (ab sempre cria, então a ação de fechar tem que estar fora do else pra acontecer sempre)
 }
 
 void mostra(loja *p,int qtde)
 {
-int i;
+int i, j;
 FILE *fptr=NULL;
 system("cls");
 if((fptr=fopen("concessionaria.bin","rb"))==NULL)
@@ -144,9 +248,15 @@ else
   	  {
 	  	fseek(fptr,i*sizeof(loja),0);
 	  	fread(p,sizeof(loja),1,fptr);
-	  	printf("\nRegistro: %i\nNome: %s\nCNPJ: %s\n",p->regloja,p->nome,p->CNPJ);
+        if(i==0)
+            printf("------------------------------------------------------------------------------------------------------------------\n");
+	  	printf("\t\t\t\t\t\t%s", (p->nome)); //toupper
+	  	printf("\nRegistro: %i\nCNPJ: %s\nLogradouro: %s\nBairro: %s\nCEP: %s\nCidade: %s\nEstado: %s\nTelefone: %s\nEmail: %s\nSold: %i\nReserved: %i\n",p->regloja,p->CNPJ,p->end.logradouro,p->end.bairro,p->end.CEP,p->end.cidade,p->end.estado,p->end.fone,p->end.email, p->sold, p->reserved);
+        for(j=0;j<3;j++)
+            printf("Tabela %i = %c\n", j, (p->tabela.sigla));
+        printf("------------------------------------------------------------------------------------------------------------------\n");
       }
-    fclose(fptr);   //dentro do else - por conta rb
+    fclose(fptr);   //dentro do else - por conta rb (rb não tem força pra criar, então a ação de fechar só deve acontecer dentro do else, caso o arquivo exista)
   }
 printf("\n\n");
 system("pause");
